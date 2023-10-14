60b90dd41ef95055cd5929da4898d446cbafc5d8
naisanzaa <naisanza@gmail.com>
2023-10-13 20:05:14 -0500

    enable enable-execute-command for ECS service

--- entry.sh
+++ entry.sh
@@ -2,11 +2,29 @@
 
 # entrypoint
 
-set -x
+set -xe
 
 env
 
 find /root/.aws -type f
 #find /root/.aws -type f | xargs cat
+
+#aws ecs describe-tasks \
+#    --cluster $cluster \
+#    --tasks $task | grep enableExecuteCommand
+
+#aws ecs update-service \
+#  --cluster $cluster \
+#  --service $service \
+#  --task-definition $task_definition \
+#  --force-new-deployment \
+#  --enable-execute-command
+
+#aws ecs execute-command \
+#    --cluster $cluster \
+#    --task $task \
+#    --container $container \
+#    --interactive \
+#    --command "/bin/bash"
 
 aws $@
--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ cd $(dirname $0); set -xe
 bash build.sh
 
 docker volume create aws-cli
-docker run --rm -it -v aws-cli:/root/.aws/ --name aws-cli --entrypoint bash aws-cli:latest $@
+docker run --rm -it --env-file env.sh -v aws-cli:/root/.aws/ --name aws-cli --entrypoint bash aws-cli:latest $@
